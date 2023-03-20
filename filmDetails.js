321778852734aa3cb3772b334b1286b1d77030ec
OceanMan73 <127486344+OceanMan73@users.noreply.github.com>
2023-03-20 13:03:07 +0100

    Update filmDetails.js

--- filmDetails.js
+++ filmDetails.js
@@ -1,14 +1,19 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const filmID = urlParams.get('film')
-console.log(filmID);
+    console.log(filmID);
 
-    const film = fetch(`https://swapi.dev/api/films/${filmID}`, {
-        method: 'GET'
+        const film = fetch(`https://swapi.dev/api/films/${filmID}`, {
+            method: 'GET'
     })
-    .then(response => response.json())
+    .then(film => film.json())
     .then(data => {
-    data.characters.forEach(character => {
+    data.characters.forEach(name => {
+     const people = fetch('https://swapi.dev/api/people/', {
+        method: 'GET'
+     });
+
+     console.log(people);
 
      const ul = document.querySelector("#list");
      const li = document.createElement("li");
@@ -16,11 +21,12 @@ console.log(filmID);
     
      ul?.append(li)
      li.append(btn);
-     btn.textContent = character;
-     const peopleID = character.substring(29,31);
+     btn.textContent = people;                    
+     const peopleID = name.substring(29,31);
      btn.addEventListener('click', function() {
         alert(`https://swapi.dev/api/people/${peopleID}`);
-    })
+        
     });
-    })
-          
+    });                                             
+    });
+
